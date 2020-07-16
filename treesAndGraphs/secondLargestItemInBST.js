405107c147419fb184dd26edde09c6ee75086e07
Danny (vai) San <vai.quoc.san@gmail.com>
2020-07-16 10:38:12 -0700

    added interview cake answer

--- treesAndGraphs/secondLargestItemInBST.js
+++ treesAndGraphs/secondLargestItemInBST.js
@@ -16,35 +16,74 @@ class BinaryTreeNode {
   }
 }
 
-function findSecondLargest(treeRoot) {
+// ** My Answer **
 
-  // Find the second largest item in the binary search tree
-  if(!treeRoot.left && !treeRoot.right){
-    throw new Error('not enough nodes')
-  }
+// function findSecondLargest(treeRoot) {
+
+//   // Find the second largest item in the binary search tree
+//   if(!treeRoot.left && !treeRoot.right){
+//     throw new Error('not enough nodes')
+//   }
   
-  const stack = []
-  stack.push(treeRoot.value)
+//   const stack = []
+//   stack.push(treeRoot.value)
   
-  const helper = (node) => {
-    if(node.value === null){
-      return
-    }
-    stack.push(node.value)
-    if(node.left){
-      helper(node.left)
-    }
-    if(node.right){
-      helper(node.right)
-    }
+//   const helper = (node) => {
+//     if(node.value === null){
+//       return
+//     }
+//     stack.push(node.value)
+//     if(node.left){
+//       helper(node.left)
+//     }
+//     if(node.right){
+//       helper(node.right)
+//     }
+//   }
+//   helper(treeRoot)
+//   stack.sort((a,b) => {
+//     return a - b
+//   })
+//   return stack[stack.length - 2]
+// }
+
+// *Interviewcake answer 
+function findLargest(rootNode) {
+  let current = rootNode;
+  while (current) {
+    if (!current.right) return current.value;
+    current = current.right;
   }
-  helper(treeRoot)
-  stack.sort((a,b) => {
-    return a - b
-  })
-  return stack[stack.length - 2]
 }
 
+function findSecondLargest(rootNode) {
+  if (!rootNode || (!rootNode.left && !rootNode.right)) {
+    throw new Error('Tree must have at least 2 nodes');
+  }
+
+  let current = rootNode;
+
+  while (current) {
+
+    // Case: current is largest and has a left subtree
+    // 2nd largest is the largest in that subtree
+    if (current.left && !current.right) {
+      return findLargest(current.left);
+    }
+
+    // Case: current is parent of largest, and largest has no children,
+    // so current is 2nd largest
+    if (
+      current.right
+      && !current.right.left
+      && !current.right.right
+    ) {
+      return current.value;
+    }
+
+    current = current.right;
+  }
+}
 
 // Tests
 
