5e84e1760a702dddf7541538abd2d608babcc100
Danny (vai) San <vai.quoc.san@gmail.com>
2020-07-22 10:43:37 -0700

    meshMessage. Answer, but need more practice / understanding

--- treesAndGraphs/meshMessage.js
+++ treesAndGraphs/meshMessage.js
@@ -16,43 +16,65 @@ class Queue {
   }
 }
 
-unction bfsGetPath(graph, startNode, endNode) {
+function getPath(graph, startNode, endNode) {
 
   // Find the shortest route in the network between the two users
   //edge cases, if startNode & endNode is not a property of graph, return null
+  if(!graph.hasOwnProperty(startNode)){
+    throw new Error('start node not in graph');
+  }
+  if(!graph.hasOwnProperty(endNode)){
+    throw new Error('end node not in graph');
+  }
   
   //create a new queue to hold nodes to check
+  const nodesToCheck = new Queue();
     //enqueue start node
+  nodesToCheck.enqueue(startNode);
   
   //create a object to track how we reached the endNode
-  
+  const howToReachEndNode = {};
   //assign startNode with value null to tracking object
-  
+  howToReachEndNode[startNode] = null;
   //while queue size is not 0
-    
+  while(nodesToCheck.size > 0){
     //assign currentNode as dequeue value
-    
+    let currentNode = nodesToCheck.dequeue();
     //if currentNode equals endNode
+    if(currentNode === endNode){
       //pass tracking object into a function that creates an array out of the property and values
-    
+      return generateArray(howToReachEndNode, startNode, endNode)
+    }
     //iterate over graph at current object's values
+    graph[currentNode].forEach(number => {
       //if tracking object doesn't have property of current element
+      if(!howToReachEndNode.hasOwnProperty(number)){
         // push current element into queue
+        nodesToCheck.enqueue(number)
         // assign tracking obeject's key of current element to currentNode
-
+        howToReachEndNode[number] = currentNode;
+      }
+    })
+  }
   return null;
 }
 
 function generateArray(nodesToEnd, start, end) {
   //create array to hold nodesToEnd elements
+  const nodesToEndInReverse = [];
   //assign currentNode var as end
-  
+  let currentNode = end
   //while currentNode is not null
+  while(currentNode !== null){
     //push current node into array
+    nodesToEndInReverse.push(currentNode)
+    currentNode = nodesToEnd[currentNode]
     //currentNode = nodesToEnd[currentNode]
-  
+  }
+  return nodesToEndInReverse.reverse()
   //return array.reverse() <--- since array is being filled in from end to start  
 }
+
 
 
 
