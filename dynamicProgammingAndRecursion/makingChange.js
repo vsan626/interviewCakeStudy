b16e739552d5bdeda60f1b5a79bc05e685e049d2
Danny (vai) San <vai.quoc.san@gmail.com>
2020-07-30 19:45:59 -0700

    making change. Needs reviewing for better understanding

---
+++ dynamicProgammingAndRecursion/makingChange.js
@@ -0,0 +1,58 @@
+function changePossibilities(amountLeft, denominations) {
+  // Calculate the number of ways to make change
+  // Initialize an array of zeros with indices up to amount
+  const waysOfDoingNcents = new Array(amountLeft + 1).fill(0);
+  waysOfDoingNcents[0] = 1;
+
+  denominations.forEach((coin) => {
+    for (let higherAmount = coin; higherAmount <= amountLeft; higherAmount++) {
+      const higherAmountRemainder = higherAmount - coin;
+      waysOfDoingNcents[higherAmount] +=
+        waysOfDoingNcents[higherAmountRemainder];
+    }
+  });
+
+  return waysOfDoingNcents[amountLeft];
+
+  return 0;
+}
+
+// Tests
+
+let desc = 'sample input';
+let actual = changePossibilities(4, [1, 2, 3]);
+let expected = 4;
+assertEqual(actual, expected, desc);
+
+desc = 'one way to make zero cents';
+actual = changePossibilities(0, [1, 2]);
+expected = 1;
+assertEqual(actual, expected, desc);
+
+desc = 'no ways if no coins';
+actual = changePossibilities(1, []);
+expected = 0;
+assertEqual(actual, expected, desc);
+
+desc = 'big coin value';
+actual = changePossibilities(5, [25, 50]);
+expected = 0;
+assertEqual(actual, expected, desc);
+
+desc = 'big target amount';
+actual = changePossibilities(50, [5, 10]);
+expected = 6;
+assertEqual(actual, expected, desc);
+
+desc = 'change for one dollar';
+actual = changePossibilities(100, [1, 5, 10, 25, 50]);
+expected = 292;
+assertEqual(actual, expected, desc);
+
+function assertEqual(a, b, desc) {
+  if (a === b) {
+    console.log(`${desc} ... PASS`);
+  } else {
+    console.log(`${desc} ... FAIL: ${a} != ${b}`);
+  }
+}
